1d55e6d14ee33ef2951d13b989260e0df2211039
Brendan McShane <brendanmcs7@gmail.com>
2021-02-10 10:03:15 -0500

    Misc changes

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = (homebridge) => {
     Service = homebridge.hap.Service
     Characteristic = homebridge.hap.Characteristic
 
-    homebridge.registerAccessory('homebridge-DCH-S160', SirenSwitchAccessory)
+    homebridge.registerAccessory('homebridge-DCH-S160', 'DCH-S160', WaterSensorAccessory)
 }
 
-class SirenSwitchAccessory {
+class WaterSensorAccessory {
     constructor(log, config) {
         this.log = log
         this.config = config || {};
@@ -24,18 +24,14 @@ class SirenSwitchAccessory {
 
         //this.service = new Service.Switch(this.config.name);
 
-        this.service = new this.Service(this.Service.ContactSensor);
+        this.service = new this.Service(this.Service.LeakSensor);
     }
 
     getServices() {
         const informationService = new Service.AccessoryInformation()
-            .setCharacteristic(Characteristic.Manufacturer, 'D-Link')
-            .setCharacteristic(Characteristic.Model, 'DCH-S160')
-            .setCharacteristic(Characteristic.SerialNumber, 'dlink-DCH-S160')
-
-        this.service.getCharacteristic(Characteristic.On)
-            .on('get', this.getOnCharacteristicHandler.bind(this))
-            .on('set', this.setOnCharacteristicHandler.bind(this));
+            .setCharacteristic(Characteristic.Manufacturer, 'd-link')
+            .setCharacteristic(Characteristic.Model, 'dch-S160')
+            .setCharacteristic(Characteristic.SerialNumber, 'dlink-dch-S160')
 
         // :: Fault status (Home does not seem to support it, but Eve app does)
         this.service.addOptionalCharacteristic(Characteristic.StatusFault);
@@ -44,7 +40,7 @@ class SirenSwitchAccessory {
         if (this.updateInterval) {
             setInterval(() => {
                 this.getStatus().then(status => {
-                    this.service.getCharacteristic(Characteristic.On).setValue(status);
+                    this.service.getCharacteristic(this.Characteristic.LeakDetected).setValue(status);
                 }).catch(err => {
                     console.log(":: Error from status polling: " + err);
                 });
@@ -55,18 +51,18 @@ class SirenSwitchAccessory {
     }
 
     /*
-     * Get the "playing" status of the siren
+     * Check to see if detector senses water:
      */
     getStatus() {
         return new Promise((resolve, reject) => {
             // :: Log In to the Siren
-            this.sirenClient.login().then(status => {
+            this.detectorClient.login().then(status => {
                 if (status !== 'success') {
                     this.log(":: An error occurred while logging in to the siren, please check the credentials in config.");
                     return reject("Error while logging in to the siren.");
                 }
                 // :: Retrieve playing status
-                this.sirenClient.getPlayingStatus().then(status => {
+                this.detectorClient.getWaterStatus().then(status => {
                     return resolve(status);
                 }).catch(err => {
                     this.log(":: An error occurred while retrieving siren status: " + err);
